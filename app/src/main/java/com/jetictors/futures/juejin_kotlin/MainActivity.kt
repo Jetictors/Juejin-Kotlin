98832809706d803956d5dba00c172cccbf1a73af
Administrator <zxc@funjoytech.com>
2018-01-26 19:56:42 +0800

    登陆模块布局

--- app/src/main/java/com/jetictors/futures/juejin_kotlin/MainActivity.kt
+++ app/src/main/java/com/jetictors/futures/juejin_kotlin/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.jetictors.futures.juejin_kotlin
 
-import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : Activity() {
 
+
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
